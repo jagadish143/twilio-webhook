2b65b4bd7db01e837821e183bbfd97c8dabee3c9
Jagadish Prasad Das <successjagadish123@gmail.com>
2023-07-19 22:53:41 +0530

    changed frontend

--- client/src/components/FetchingData/GetData.js
+++ client/src/components/FetchingData/GetData.js
@@ -2,12 +2,14 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import MessageBox from './MessageBox';
 
+const hostName = process.env.REACT_APP_API_KEY;
+
 const GetData = () => {
   const [data, setData] = useState([]);
   useEffect(() => {
     const fetchData = async () => {
       console.log('Fetching Data');
-      const addData = await axios.get('/');
+      const addData = await axios.get(`${hostName}/`);
       console.log(addData.data.data.data);
       setData(addData.data.data.data);
     };
